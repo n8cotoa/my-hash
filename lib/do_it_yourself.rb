9f05820319eabfcd3e82ccfd88a548b4b601b9e1
Nate Cottle and Reese Glasscock <nate@>
2018-07-02 15:34:09 -0700

    Add a lot of stuff.

--- lib/do_it_yourself.rb
+++ lib/do_it_yourself.rb
@@ -1,16 +1,35 @@
 class MyHash
-  attr_accessor(:custom_hash)
+  attr_accessor(:custom_keys, :custom_values)
 
-  def initialize ()
-    @custom_hash = {}
+  def initialize
+    @custom_keys = []
+    @custom_values = []
+    @custom_hash = []
+    # @custom_hash = @custom_keys.zip(@custom_values)
   end
 
   def my_store(key, value)
-    @custom_hash[key] = value
+    @custom_keys.push(key)
+    @custom_values.push(value)
+    value_index = 0
+    # binding.pry
+    @custom_keys.each do |key|
+      if @custom_hash.flatten.include?(key)
+        value_index += 1
+      else
+        @custom_hash.push([key, @custom_values[value_index]])
+        value_index += 1
+      end
+    end
   end
 
-  def my_fetch(key)
-    fetch_hash = @custom_hash[key]
+  def my_fetch(input)
+    input_index = @custom_hash.flatten.index(input)
+    if input_index % 2 == 0
+      @custom_hash.flatten.fetch(input_index + 1)
+    else
+      "party time"
+    end
   end
 
   def has_my_value?(value)
@@ -27,6 +46,16 @@ class MyHash
     else
       false
     end
+  end
+
+  def my_length
+    length = @custom_hash.length
+  end
+
+  def my_merge(hash)
+    merged = (@custom_hash & hash).to_h
+    merged
+
   end
 
 end
--- spec/do_it_yourself_spec.rb
+++ spec/do_it_yourself_spec.rb
@@ -14,14 +14,29 @@ describe ('#do_it_yourself') do
     new_hash.my_store("cheese", "mouse")
     expect(new_hash.my_fetch("cheese")).to(eq("mouse"))
   end
-  it "Takes in a value and returns true if value exists" do
-    new_hash = MyHash.new()
-    new_hash.my_store("peanut", "elephant")
-    expect(new_hash.has_my_value?("elephant")).to(eq(true))
-  end
-  it "Takes in a key and returns true if key exists" do
-    new_hash = MyHash.new()
-    new_hash.my_store("peanut", "elephant")
-    expect(new_hash.has_my_key?("peanut")).to(eq(true))
-  end
+  # it "Takes in a value and returns true if value exists" do
+  #   new_hash = MyHash.new()
+  #   new_hash.my_store("peanut", "elephant")
+  #   expect(new_hash.has_my_value?("elephant")).to(eq(true))
+  # end
+  # it "Takes in a key and returns true if key exists" do
+  #   new_hash = MyHash.new()
+  #   new_hash.my_store("peanut", "elephant")
+  #   expect(new_hash.has_my_key?("peanut")).to(eq(true))
+  # end
+  # it "Returns the length of the hash to the user" do
+  #   new_hash = MyHash.new()
+  #   new_hash.my_store("peanut", "elephant")
+  #   new_hash.my_store("cheese", "mouse")
+  #   expect(new_hash.my_length).to(eq(2))
+  # end
+  # it "Joins two hashes together" do
+  #   new_hash = MyHash.new()
+  #   new_hash.my_store("peanut", "elephant")
+  #   new_hash.my_store("cheese", "mouse")
+  #   other_hash = MyHash.new()
+  #   other_hash.my_store("cheese", "mouse")
+  #   other_hash.my_store("apple", "horse")
+  #   expect(new_hash.my_merge(other_hash)).to(eq("yes"))
+  # end
 end
